0011aca2c2bd4b8480bfdb82c07c32066e31214f
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2022-04-25 08:03:39 +0200

    Update client.c

--- client.c
+++ client.c
@@ -7,7 +7,7 @@ t_mini	*client_initiate(void)
 	talk = malloc(sizeof(mini));
 	if (!talk)
 	{
-		ft_putstr("ERROR!, malloc failed :\( !\n");
+		ft_putstr("ERROR!, malloc failed :( !\n");
 		exit(EXIT_FAILURE);
 	}
 	talk->pid_server = 0;
@@ -17,7 +17,24 @@ t_mini	*client_initiate(void)
 
 void	client_send(t_mini *talk, char *message)
 {
-
+	int	bit_displacement;
+	int	i;
+	int	signal;
+	
+	i = -1;
+	while(++i <= ft_strlen(message))
+	{
+		bit_desplacement = -1;
+		signal = 0;
+		while (++bit_displacement < 7)
+		{
+			if((message[i] >> bit_displacement) & 1)
+				signal = SIGUSR2;
+			else
+				signal = SIGUSR1;
+			kill(talk->pid_server, signal);
+			usleep(100);
+		}
 	}
 	return ;
 }
@@ -29,15 +46,21 @@ int	main(int nword, char *arguments[])
 	talk = NULL;
 	if (nword != 3)
 	{
-		ft_putstr("ERROR!, Use \"./server\" with 3 argument please :\)\n");
+		ft_putstr("ERROR!, Use \"./server\" with 3 argument please :)\n");
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		talk = client_initiate();
-		talk->pid_client = getpid();
+		talk->pid_server = ft_atoi(arguments[1]);
+		if (talk->pid_server <= 0)
+		{
+			ft_putstr("ERROR!, PID is equal or less than 0 x-x!\n");
+			free(talk);
+			exit(EXIT_FAILURE);
+		}
+		client_send(talk, arguments[2]);
 	}
-	
 	free(talk);
 	return (EXIT_SUCCESS);
 }
